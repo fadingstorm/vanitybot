72276d6d588f337468ce62f2e425744bbe739a6e
Fadingstorm <mcclellangareth@gmail.com>
2023-06-29 23:53:37 +0400

    Created a couple commands, made them slash commands. Gonna go to sle ep now.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+import lists
 import settings
 import discord
 from discord.ext import commands
@@ -5,14 +7,32 @@ from discord.ext import commands
 logger = settings.logging.getLogger("bot")
 
 def run():
-    intents = discord.Intents.default()
+    intents = discord.Intents.all()
 
     bot = commands.Bot(command_prefix='v!', intents=intents)
+    bot.remove_command('help') # remove the default help command, it's ugly
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
-        print('woah')
+        print('WOAH')
+        logger.info(f"Guild ID: {bot.guilds[0].id}")
+        bot.tree.copy_global_to(guild=settings.GUILDS_ID)
+        await bot.tree.sync(guild=settings.GUILDS_ID)
+
+
+   # Here are all the commands for the bot
+    @bot.command()
+    async def help(ctx):
+        await ctx.send("this is the help page") 
+    
+    @bot.tree.command(description='Ask a yes or no question!', name='fate')
+    async def fate(interaction: discord.Interaction, question: str):
+        await interaction.response.send_message(random.choice(lists.eight_ball_responses))
+
+    @bot.tree.command(description='Says pong!', name='ping')
+    async def ping(interaction: discord.Interaction):
+        await interaction.response.send_message(f"pong! {interaction.user.mention}", ephemeral=True)
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
--- settings.py
+++ settings.py
@@ -2,10 +2,14 @@ import os
 import logging
 from logging.config import dictConfig
 from dotenv import load_dotenv
+import discord
+
 
 load_dotenv()
 
 DISCORD_API_SECRET = os.getenv("DISCORD_API_TOKEN")
+
+GUILDS_ID = discord.Object(id=int(os.getenv("GUILD")))
 
 LOGGING_CONFIG = {
     "version" : 1,
